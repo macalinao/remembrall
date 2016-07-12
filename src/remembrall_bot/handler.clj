c80d61893085e1f567f5ed2f06feca7747346b91
Ian Macalinao <me@ian.pw>
2016-07-12 03:13:22 -0400

    setup webhook

--- src/remembrall_bot/handler.clj
+++ src/remembrall_bot/handler.clj
@@ -1,17 +1,16 @@
 (ns remembrall-bot.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [ring.adapter.jetty :as ring]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/asdf" [] "Hello World")
   (POST "/webhook" [] "Hello my little friend")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes api-defaults))
 
 (defn start [port]
   (ring/run-jetty app {:port port
