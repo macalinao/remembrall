b0f0b730d6e17b8f4f82ea3777971b239d4193ea
Ian Macalinao <me@ian.pw>
2016-07-12 03:57:27 -0400

    Implement webhook

--- src/remembrall_bot/handler.clj
+++ src/remembrall_bot/handler.clj
@@ -1,16 +1,27 @@
 (ns remembrall-bot.handler
   (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [ring.adapter.jetty :as ring]))
 
+(def verify-token "orworse_exp3lled!")
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (POST "/webhook" [] "Hello my little friend")
+  (GET "/webhook" {params :params}
+    (if
+        (and
+         (= (get params "hub.mode") "subscribe")
+         (= (get params "hub.verify_token") verify-token))
+      {:status 200 :body (get params "hub.challenge")}
+      {:status 403 :body "Failed validation."}))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes api-defaults))
+  (-> app-routes
+      (handler/api)
+      (wrap-defaults api-defaults)))
 
 (defn start [port]
   (ring/run-jetty app {:port port
