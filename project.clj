1ae33ff4a33c57eeec8e78999d52ec9e6e8cb457
Ian Macalinao <me@ian.pw>
2016-07-12 02:49:25 -0400

    Make things work via lein run

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  [ring/ring-defaults "0.2.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler remembrall-bot.handler/app}
+  :main ^:skip-aot remembrall-bot.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
--- src/remembrall_bot/handler.clj
+++ src/remembrall_bot/handler.clj
@@ -1,7 +1,8 @@
 (ns remembrall-bot.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.adapter.jetty :as ring]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
@@ -9,3 +10,11 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn start [port]
+  (ring/run-jetty app {:port port
+                               :join? false}))
+
+(defn -main []
+  (let [port (Integer. (or (System/getenv "PORT") "8080"))]
+    (start port)))
