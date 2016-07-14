ee4149492c85c2e3bbc6e415cf89c89c753313b9
Ian Macalinao <me@ian.pw>
2016-07-14 00:11:11 -0400

    Implement messenger echo server

--- project.clj
+++ project.clj
@@ -5,7 +5,13 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [clj-http "2.2.0"]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [environ "1.0.3"]]
+
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler remembrall-bot.handler/app}
   :main ^:skip-aot remembrall-bot.handler
--- src/remembrall_bot/handler.clj
+++ src/remembrall_bot/handler.clj
@@ -2,13 +2,25 @@
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]
+            [ring.middleware.json :refer [wrap-json-body]]
+            [remembrall-bot.messenger :refer :all]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [ring.adapter.jetty :as ring]))
+            [ring.adapter.jetty :as ring]
+            [clojure.core.match :refer [match]]
+            [environ.core :refer [env]]))
 
-(def verify-token "orworse_exp3lled!")
+(def access-token (env :access-token))
+(def verify-token (env :verify-token))
+
+(defn respond-message [{:keys [message sender recipient timestamp]}]
+  (send-text {:access-token access-token
+              :sender sender
+              :text message}))
+
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+
   (GET "/webhook" {params :params}
     (if
         (and
@@ -16,12 +28,19 @@
          (= (get params "hub.verify_token") verify-token))
       {:status 200 :body (get params "hub.challenge")}
       {:status 403 :body "Failed validation."}))
+
+  (POST "/webhook" {body :body}
+    (match body
+           [{:message _}] (respond-message body)
+           :else {:status 400 :body "Invalid "}))
+
   (route/not-found "Not Found"))
 
 (def app
   (-> app-routes
       (handler/api)
-      (wrap-defaults api-defaults)))
+      (wrap-defaults api-defaults)
+      (wrap-json-body {:keywords? true :bigdecimals? true})))
 
 (defn start [port]
   (ring/run-jetty app {:port port
